6f3b5a5312e83b844719725f04d3bc8e3aec38bc
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-12 14:25:31 +0300

    Add function to validate octets

--- ipAddressValidation.cpp
+++ ipAddressValidation.cpp
@@ -19,3 +19,27 @@ string get_address_part(const string& ip, int part) {
     }
     return ""; // If not found octet
 }
+
+// Function for checking the correctness of octomet
+bool is_valid_octet(const string& octet) {
+    //Check the length of the octomet
+    if (octet.length() == 0 || octet.length() > 3) {
+        return false;
+    }
+
+    // Check the presence of leading zeros
+    if (octet.length() > 1 && octet[0] == '0') {
+        return false;
+    }
+
+    // We check that all characters are numbers
+    for (char c : octet) {
+        if (c < '0' || c > '9') {
+            return false;
+        }
+    }
+
+    // We convert the line into the number and check the range
+    int value = stoi(octet);
+    return value >= 0 && value <= 255;
+}
