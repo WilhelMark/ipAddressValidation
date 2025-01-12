ce503186fdfadbee409361c6909139c3b2aa8f39
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-12 14:19:37 +0300

    Add function to extract individual octets from IP address

---
+++ ipAddressValidation.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+// Function for obtaining a separate part of the IP address (octomet)
+string get_address_part(const string& ip, int part) {
+    stringstream ss(ip);
+    string token;
+    int count = 0;
+
+    // Divide the line by points
+    while (getline(ss, token, '.')) {
+        if (count == part) {
+            return token; // Return the desired octet
+        }
+        count++;
+    }
+    return ""; // If not found octet
+}
